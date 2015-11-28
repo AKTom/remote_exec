375cbd608f2aed1e052a7ee3aee00db6a83d04dd
meilin.li <meilin.li@meilinli.local>
2015-11-28 15:32:34 +0800

    2015-11-28 15:32

--- start.py
+++ start.py
@@ -108,4 +108,4 @@ if __name__ == '__main__':
     while 1:
         data=[]
         getHost()
-        man()
+        if data!=[]:man()
--- tab.py
+++ tab.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python
-import readline,rlcompleter
-
-### Indenting
-class TabCompleter(rlcompleter.Completer):
-    """Completer that supports indenting"""
-    def complete(self, text, state):
-        if not text:
-            return ('    ', None)[state]
-        else:
-            return rlcompleter.Completer.complete(self, text, state)
-
-readline.set_completer(TabCompleter().complete)
-
-### Add autocompletion
-if 'libedit' in readline.__doc__:
-    readline.parse_and_bind("bind -e")
-    readline.parse_and_bind("bind '\t' rl_complete")
-else:
-    readline.parse_and_bind("tab: complete")
-
-### Add history
+import sys
+import readline
+import rlcompleter
+import atexit
 import os
-histfile = os.path.join(os.environ["HOME"], ".pyhist")
+# tab completion
+readline.parse_and_bind('tab: complete')
+# history file
+histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
 try:
     readline.read_history_file(histfile)
 except IOError:
     pass
-import atexit
 atexit.register(readline.write_history_file, histfile)
-del histfile
+ 
+del os, histfile, readline, rlcompleter
